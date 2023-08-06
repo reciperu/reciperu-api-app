c3b34f2d7f77d5879c0d26b4f0b00db3758055e6
masatotezuka <tez.0731.mst@gmail.com>
2023-08-07 00:17:30 +0900

    feat demo

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { FirebaseService } from 'src/firebase/firebase.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [UserController],
-  providers: [FirebaseService],
+  providers: [FirebaseService, PrismaService],
 })
 export class UserModule {}
