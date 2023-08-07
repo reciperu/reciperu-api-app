6827e4a5d666cc95a7c6be10d470b335332e374a
masatotezuka <tez.0731.mst@gmail.com>
2023-08-07 23:35:18 +0900

    create user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,43 +1,16 @@
-import { Controller, Get, Req, Post, Body } from '@nestjs/common';
-import { Public } from 'src/auth/decorators/public.decorator';
+import { Controller, Req, Post } from '@nestjs/common';
 import { Request } from 'express';
-import { FirebaseService } from 'src/firebase/firebase.service';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { UserService } from './user.service';
+import { ApiOperation } from '@nestjs/swagger';
 
-// あとで修正
 @Controller('users')
 export class UserController {
-  constructor(
-    private readonly firebaseService: FirebaseService,
-    private readonly prismaService: PrismaService,
-  ) {}
-  @Public()
-  @Get()
-  getHello() {
-    return 'Hello World!';
-  }
-
-  @Public()
+  constructor(private readonly userService: UserService) {}
   @Post()
-  async createUser(@Body() body: { name: string; imageUrl: string }) {
-    const user = await this.prismaService.user.create({
-      data: {
-        name: body.name,
-        imageUrl: body.imageUrl,
-        isOwner: false,
-        uuid: 'uuid',
-        spaceId: undefined,
-      },
-    });
-    return user;
-  }
-  @Get('auth')
-  async getAuth(@Req() request: Request) {
+  @ApiOperation({ operationId: 'createUser' })
+  async create(@Req() request: Request) {
     const token = request.headers.authorization.split(' ')[1];
-    const decodedToken = await this.firebaseService.admin
-      .auth()
-      .verifyIdToken(token);
-    console.log(decodedToken.name);
-    return 'Auth OK!';
+    await this.userService.create(token);
+    return;
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { FirebaseService } from 'src/firebase/firebase.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [FirebaseService, PrismaService],
+  providers: [FirebaseService, PrismaService, UserService],
 })
 export class UserModule {}
