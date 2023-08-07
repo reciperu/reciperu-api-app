5318169e8349ec2be3ccf8b92ac9e1481262b451
masatotezuka <tez.0731.mst@gmail.com>
2023-08-07 23:34:26 +0900

    install swagger openapi

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as fs from 'fs';
+import { dump } from 'js-yaml';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3040);
+
+  const config = new DocumentBuilder()
+    .setTitle('reciperu api app')
+    .setDescription('API documents')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  fs.writeFileSync('./openapi.yaml', dump(document, {}));
+  SwaggerModule.setup('api', app, document);
+
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  await app.listen(3333);
 }
 bootstrap();
