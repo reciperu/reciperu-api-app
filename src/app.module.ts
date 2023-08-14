1f77ce20abd1b551823e6df5602e6237d2e82f52
masatotezuka <tez.0731.mst@gmail.com>
2023-08-15 01:24:56 +0900

    feat ClassSerializerInterceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,15 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { FirebaseModule } from './firebase/firebase.module';
 import { SpaceModule } from './space/space.module';
-
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ClassSerializerInterceptor } from '@nestjs/common';
 @Module({
   imports: [UserModule, AuthModule, PrismaModule, FirebaseModule, SpaceModule],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
