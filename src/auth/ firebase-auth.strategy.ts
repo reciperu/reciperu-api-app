1c701c8855a00ca1dc842f85722d7868338951f3
masatotezuka <tez.0731.mst@gmail.com>
2023-08-20 19:13:07 +0900

    fix FirebaseStrategy

--- src/auth/ firebase-auth.strategy.ts
+++ src/auth/ firebase-auth.strategy.ts
@@ -1,18 +1,20 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-http-bearer';
-import * as admin from 'firebase-admin';
+import { FirebaseService } from 'src/firebase/firebase.service';
 
 @Injectable()
 export class FirebaseStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly firebaseService: FirebaseService) {
     super();
   }
   // HTTP Bearerストラテジがtokenを渡してくれる
   // node_modules/@nestjs/passport/dist/passport/passport.strategy.js:11:55で呼び出しされている
   async validate(token: string) {
     try {
-      const decodedToken = await admin.auth().verifyIdToken(token);
+      const decodedToken = await this.firebaseService.admin
+        .auth()
+        .verifyIdToken(token);
       return decodedToken;
     } catch (error) {
       throw new UnauthorizedException();
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { PassportModule } from '@nestjs/passport';
 import { FirebaseStrategy } from './ firebase-auth.strategy';
 import { FirebaseAuthGuard } from './ firebase-auth.guard';
 import { APP_GUARD } from '@nestjs/core';
+import { FirebaseService } from 'src/firebase/firebase.service';
 
 @Module({
   imports: [PassportModule],
@@ -12,6 +13,7 @@ import { APP_GUARD } from '@nestjs/core';
       provide: APP_GUARD,
       useClass: FirebaseAuthGuard,
     },
+    FirebaseService,
   ],
 })
 export class AuthModule {}
