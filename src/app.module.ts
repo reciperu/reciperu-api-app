0661c510442ea066707c319f34e556adfcdba06c
masatotezuka <tez.0731.mst@gmail.com>
2023-08-29 22:34:50 +0900

    feat supabase module

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,9 +12,21 @@ import { SpaceModule } from './space/space.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ClassSerializerInterceptor } from '@nestjs/common';
 import { SetCurrentUserMiddleware } from './setCurrentUser.middleware';
+import { SupabaseModule } from './supabase/supabase.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, AuthModule, PrismaModule, FirebaseModule, SpaceModule],
+  imports: [
+    UserModule,
+    AuthModule,
+    PrismaModule,
+    FirebaseModule,
+    SpaceModule,
+    SupabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   providers: [
     {
       provide: APP_INTERCEPTOR,
