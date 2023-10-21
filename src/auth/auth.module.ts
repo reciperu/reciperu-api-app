f2ed71cf36a3b3227071c720bcc28d01970ad5f2
masatotezuka <tez.0731.mst@gmail.com>
2023-10-21 16:28:17 +0900

    commnet out

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,19 @@
-import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
-import { FirebaseStrategy } from './ firebase-auth.strategy';
-import { FirebaseAuthGuard } from './ firebase-auth.guard';
-import { APP_GUARD } from '@nestjs/core';
-import { FirebaseService } from 'src/firebase/firebase.service';
+// import { Module } from '@nestjs/common';
+// import { PassportModule } from '@nestjs/passport';
+// import { FirebaseStrategy } from './ firebase-auth.strategy';
+// import { FirebaseAuthGuard } from './ firebase-auth.guard';
+// import { APP_GUARD } from '@nestjs/core';
+// import { FirebaseService } from 'src/firebase/firebase.service';
 
-@Module({
-  imports: [PassportModule],
-  providers: [
-    FirebaseStrategy,
-    {
-      provide: APP_GUARD,
-      useClass: FirebaseAuthGuard,
-    },
-    FirebaseService,
-  ],
-})
-export class AuthModule {}
+// @Module({
+//   imports: [PassportModule],
+//   providers: [
+//     FirebaseStrategy,
+//     {
+//       provide: APP_GUARD,
+//       useClass: FirebaseAuthGuard,
+//     },
+//     FirebaseService,
+//   ],
+// })
+// export class AuthModule {}
