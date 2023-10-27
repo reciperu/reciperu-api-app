be4c2f550349d1bb9b4755f7acf0fc28009ce49d
masatotezuka <tez.0731.mst@gmail.com>
2023-10-28 00:55:13 +0900

    fix auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,35 +4,39 @@ import {
   MiddlewareConsumer,
   RequestMethod,
 } from '@nestjs/common';
-// import { UserModule } from './user/user.module';
-// import { AuthModule } from './auth/auth.module';
-
 import { FirebaseModule } from './infrastructure/firebase/firebase.module';
-// import { SpaceModule } from './space/space.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ClassSerializerInterceptor } from '@nestjs/common';
-
-import { MenuPresenter } from './controllers/menu/menu.presenter';
 import { ControllersModule } from './controllers';
 import { DatabaseModule } from './infrastructure/database/database.module';
 import { UseCaseModule } from './use-cases/use-case.module';
-// import { SetCurrentUserMiddleware } from './setCurrentUser.middleware';
+import { AuthModule } from './infrastructure/auth/auth.module';
+import { SetCurrentUserMiddleware } from './middleware/setCurrentUser.middleware';
+import { FirebaseService } from './infrastructure/firebase/firebase.service';
+import { PrismaService } from './infrastructure/database/prisma/prisma.service';
 
 @Module({
-  imports: [FirebaseModule, ControllersModule, DatabaseModule, UseCaseModule],
+  imports: [
+    FirebaseModule,
+    ControllersModule,
+    DatabaseModule,
+    UseCaseModule,
+    AuthModule,
+  ],
   providers: [
     {
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor,
     },
+    FirebaseService,
+    PrismaService,
   ],
 })
-export class AppModule {}
-// export class AppModule implements NestModule {
-// configure(consumer: MiddlewareConsumer) {
-//   consumer
-//     .apply(SetCurrentUserMiddleware)
-//     .exclude({ path: 'users', method: RequestMethod.POST })
-//     .forRoutes('*');
-// }
-// }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(SetCurrentUserMiddleware)
+      .exclude({ path: 'users', method: RequestMethod.POST })
+      .forRoutes('*');
+  }
+}
---
+++ src/middleware/setCurrentUser.middleware.ts
@@ -0,0 +1,26 @@
+import { HttpException, Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { PrismaService } from 'src/infrastructure/database/prisma/prisma.service';
+import { FirebaseService } from 'src/infrastructure/firebase/firebase.service';
+
+@Injectable()
+export class SetCurrentUserMiddleware implements NestMiddleware {
+  constructor(
+    private readonly prismService: PrismaService,
+    private readonly firebaseService: FirebaseService,
+  ) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+      const decodedToken = await this.firebaseService.admin
+        .auth()
+        .verifyIdToken(token);
+      req['currentUser'] = await this.prismService.user.findUnique({
+        where: { uid: decodedToken.uid },
+      });
+      next();
+    } catch (error) {
+      throw new HttpException(error, 401);
+    }
+  }
+}
