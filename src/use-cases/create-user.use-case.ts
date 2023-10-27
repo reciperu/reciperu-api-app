cbc19709122168fef7f4f22307117153f4b91f30
masatotezuka <tez.0731.mst@gmail.com>
2023-10-27 23:02:07 +0900

    add create-user use case

---
+++ src/use-cases/create-user.use-case.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/controllers/user/user.dto';
+import { IUserRepository } from 'src/domain/repositories';
+import { FirebaseService } from 'src/infrastructure/firebase/firebase.service';
+
+@Injectable()
+export class CreateUserUseCase {
+  constructor(
+    private readonly userRepository: IUserRepository,
+    private readonly firebaseService: FirebaseService,
+  ) {
+    this.userRepository = userRepository;
+  }
+  async execute(dto: CreateUserDto, token: string) {
+    const decodedToken = await this.firebaseService.admin
+      .auth()
+      .verifyIdToken(token);
+    return await this.userRepository.create({
+      ...dto,
+      identifier: decodedToken.uid,
+    });
+  }
+}
