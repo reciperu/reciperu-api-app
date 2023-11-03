b88d17f609918ce5e1d4b7c834305b12d1f3fb26
masatotezuka <tez.0731.mst@gmail.com>
2023-11-04 01:38:26 +0900

    remove createUserDto

--- src/controllers/user/user.dto.ts
+++ src/controllers/user/user.dto.ts
@@ -1,17 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
-export class CreateUserDto {
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
-  readonly name: string;
-
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
-  readonly imageUrl: string;
-}
-
 export class UpdateUserDto {
   @ApiProperty()
   @IsString()
