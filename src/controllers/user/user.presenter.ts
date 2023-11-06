7b7eb1c2a9d44e872374bb173887b1acfe8ced8e
masatotezuka <tez.0731.mst@gmail.com>
2023-11-06 23:42:57 +0900

    fix UserPresenter

--- src/controllers/user/user.presenter.ts
+++ src/controllers/user/user.presenter.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { User, ActiveStatus } from 'src/domain';
+import { User, ActiveStatus, SpaceRole } from 'src/domain';
 
 export class UserPresenter {
   @ApiProperty()
@@ -14,10 +14,14 @@ export class UserPresenter {
   @ApiProperty({ enum: ActiveStatus })
   readonly activeStatus: ActiveStatus;
 
+  @ApiProperty({ enum: SpaceRole })
+  readonly spaceRole: SpaceRole;
+
   constructor(user: User) {
     this.id = user.getId;
     this.name = user.getName;
     this.imageUrl = user.getImageUrl;
     this.activeStatus = user.getActiveStatus;
+    this.spaceRole = user.getSpaceRole;
   }
 }
