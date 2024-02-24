46b000152779d0eabd7e05ae407a28611fb730e8
Matsumoto Ryota <awpjdmga1943@icloud.com>
2024-02-24 23:54:59 +0900

    fix slack webhook

--- src/controllers/contact/contact.presenter.ts
+++ src/controllers/contact/contact.presenter.ts
@@ -1,4 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class ContactRequestBody {
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  email?: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+}
 
 export class ContactPresenter {
   @ApiProperty()
--- src/controllers/contact/index.ts
+++ src/controllers/contact/index.ts
@@ -1,3 +1,2 @@
 export * from './contact.controller';
 export * from './contact.presenter';
-export * from './contact.dto';
