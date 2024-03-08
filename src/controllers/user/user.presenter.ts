529de8f0a3d8774e670e9ccfaf8cc1bddb9743f4
masatotezuka <tez.0731.mst@gmail.com>
2024-03-08 23:27:42 +0900

    fix space id

--- src/controllers/user/user.presenter.ts
+++ src/controllers/user/user.presenter.ts
@@ -14,22 +14,18 @@ export class UserPresenter {
   @ApiProperty({ enum: ActiveStatus })
   readonly activeStatus: ActiveStatus;
 
-  @ApiProperty({ required: false })
-  readonly spaceOwnerId?: string;
+  @ApiProperty()
+  readonly spaceId: string;
 
-  @ApiProperty({ required: false })
-  readonly spaceParticipantId?: string;
+  @ApiProperty({ enum: SpaceRole })
+  readonly spaceRole: SpaceRole;
 
   constructor(user: User) {
     this.id = user.getId;
     this.name = user.getName;
     this.imageUrl = user.getImageUrl;
     this.activeStatus = user.getActiveStatus;
-    if (user.getSpaceRole === SpaceRole.PARTICIPANT) {
-      this.spaceParticipantId = user.getSpaceId;
-    }
-    if (user.getSpaceRole === SpaceRole.OWNER) {
-      this.spaceOwnerId = user.getSpaceId;
-    }
+    this.spaceRole = user.getSpaceRole;
+    this.spaceId = user.getSpaceId;
   }
 }
