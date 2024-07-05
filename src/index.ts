ea5391a9a33ef90addff078816eaf83759c35b9d
Matsumoto Ryota <awpjdmga1943@icloud.com>
2024-07-05 23:13:32 +0900

    feat: Update firebase functions setting

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,39 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as fs from 'fs';
-import { dump } from 'js-yaml';
+import * as express from 'express';
+import * as functions from 'firebase-functions';
+import helmet from 'helmet';
 import * as bodyParser from 'body-parser';
 import { ValidationPipe } from '@nestjs/common';
-import helmet from 'helmet';
+
 import { HttpExceptionFilter } from './infrastructure/filter/http-exception.filter';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+const server = express();
+
+export const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
   app.enableCors();
   app.enableShutdownHooks();
   app.useGlobalFilters(new HttpExceptionFilter());
-  app.use(helmet());
   app.use(bodyParser.json({ limit: '50mb' })); // jsonをパースする際のlimitを設定
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); // urlencodeされたボディをパースする際のlimitを設定
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  const config = new DocumentBuilder()
-    .setTitle('reciperu api app')
-    .setDescription('API documents')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .addTag('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  fs.writeFileSync('./openapi.yaml', dump(document, {}));
-  SwaggerModule.setup('api', app, document);
 
-  await app.listen(3333);
-}
-bootstrap();
+  app.use(helmet());
+
+  console.log('the server is starting @ firebase');
+  return app.init();
+};
+
+createNestServer(server)
+  .then(() => console.log('Nest Ready'))
+  .catch((err) => console.error('Nest broken', err));
+
+export const api = functions.https.onRequest(server);
