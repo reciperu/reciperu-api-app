64b0861a66ddc51b7de52c1f40d26a78f4cdea22
RyoTa <ryotanny@gmail.com>
2024-07-08 01:37:16 +0900

    change region of firebase function

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ import * as bodyParser from 'body-parser';
 import { ValidationPipe } from '@nestjs/common';
 
 import { HttpExceptionFilter } from './infrastructure/filter/http-exception.filter';
-import { PrismaService } from './infrastructure/database/prisma';
 
 const server = express();
 
@@ -17,27 +16,21 @@ export const createNestServer = async (expressInstance) => {
     AppModule,
     new ExpressAdapter(expressInstance),
   );
-
   app.enableCors();
   app.enableShutdownHooks();
   app.useGlobalFilters(new HttpExceptionFilter());
   app.use(bodyParser.json({ limit: '50mb' })); // jsonをパースする際のlimitを設定
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); // urlencodeされたボディをパースする際のlimitを設定
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   app.use(helmet());
-
-  const prismaService = app.get(PrismaService);
-  prismaService.enableShutdownHooks(app);
-
   console.log('the server is starting @ firebase');
   return app.init();
 };
 
-createNestServer(server)
-  .then(() => console.log('Nest Ready'))
-  .catch((err) => console.error('Nest broken', err));
-
-export const api = functions.https.onRequest(server);
+export const api = functions
+  .region('asia-northeast1')
+  .https.onRequest(async (request, response) => {
+    await createNestServer(server);
+    server(request, response);
+  });
