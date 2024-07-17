93566831924c5f6b09386d13234272dacfa17276
RyoTa <ryotanny@gmail.com>
2024-07-17 10:46:28 +0900

    fix error correction in case of missing firebaseConfig

--- src/functions/getEnvFilePath.ts
+++ src/functions/getEnvFilePath.ts
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 import * as functions from 'firebase-functions';
 
 export const getEnvFilePath = (envFilesDirectory: string): string => {
-  const firebaseEnv = functions.config().envs.firebase_env;
+  const firebaseEnv = functions.config().envs?.firebase_env;
   const env = firebaseEnv ? firebaseEnv : process.env.NODE_ENV || 'development'; // Default to 'development' if NODE_ENV is not set
   const fileName = `.env.${env}`;
   const filePath = resolve(`${envFilesDirectory}/${fileName}`);
