9dbac16afd7fb2fb2357efd6a72a0ce97836d90b
RyoTa <ryotanny@gmail.com>
2024-08-13 11:21:46 +0900

    feat: change library of fetching metadata

--- src/use-cases/get-recipe-meta-data.use-case.ts
+++ src/use-cases/get-recipe-meta-data.use-case.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import puppeteer from 'puppeteer';
 import { JSDOM } from 'jsdom';
+
 const FAVICON_URL = 'http://www.google.com/s2/favicons?domain=';
 @Injectable()
 export class GetRecipeMetaDateUseCase {
   async execute(recipeUrl: string) {
     const dom = await this.parseUrlToDom(recipeUrl);
     const ogImage = this.getOgpData(dom, 'og:image');
-    const ogTitle = this.getOgpData(dom, 'og:title');
+    let ogTitle = this.getOgpData(dom, 'og:title');
     const siteName = this.getSiteName(dom, recipeUrl);
     const faviconUrl = FAVICON_URL + recipeUrl;
+    if (ogTitle.includes('\n')) {
+      ogTitle = ogTitle.split('\n')[0];
+    }
     return {
       title: ogTitle,
       thumbnailUrl: ogImage,
@@ -33,13 +38,20 @@ export class GetRecipeMetaDateUseCase {
     if (recipeUrl.includes('cookpad.com')) {
       return 'クックパッド';
     }
+    // instagramの場合
+    if (recipeUrl.includes('instagram.com')) {
+      return 'Instagram';
+    }
     // note: 今後他のサイトも追加していく
     return '';
   }
 
   async parseUrlToDom(recipeUrl: string) {
-    const response = await fetch(recipeUrl);
-    const data = await response.text();
-    return new JSDOM(data);
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(recipeUrl);
+    const content = await page.content();
+    await browser.close();
+    return new JSDOM(content);
   }
 }
